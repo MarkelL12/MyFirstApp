da13800a5f792192aaafb2b5d0552324eeee0202
Mark Litvinov <mark.l.fsbc@gmail.com>
2020-10-02 10:14:22 -0500

    Changed Button Name

--- app/src/main/java/com/example/git/MainActivity.kt
+++ app/src/main/java/com/example/git/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.git
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         fun snickers(){
-            println("Hello World")
+            val candyButton:Button = findViewById<Button>(R.id.toastButton)
         }
     }
 }
